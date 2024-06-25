a727159e054af35d1eaed6142b06e5d26f073b6d
saijami <saijami@unosimple.com>
2024-06-25 18:41:37 +0530

    Todos component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
 }
 
 export default App;
